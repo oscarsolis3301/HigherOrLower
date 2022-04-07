83e8a4d15e95b7ddef0697a5ec350f1b53b14b4e
Oscar Solis <oscarsolis3301@gmail.com>
2022-04-07 15:00:03 -0600

    Completed

--- game.py
+++ game.py
@@ -8,44 +8,54 @@ score = 0
 correct = False
 
 
+def generate(generate_A, generate_B):
+    generate_A = random.randint(0, len(data) - 1)
+    generate_B = random.randint(0, len(data) - 1)
+    return generate_A, generate_B
+
+def checker():
+    user_guess = input("Who has more followers? Type 'A' or 'B': ")
+    if user_guess.lower() == 'a':
+        choice_A = data[generate_A]['follower_count']
+        choice_B = data[generate_B]['follower_count']
+        if choice_A > choice_B:
+            return True
+        else:
+            print(f"Sorry, that's wrong. Final score {score}")
+            return False
+    elif user_guess.lower() == 'b':
+        choice_A = data[generate_A]['follower_count']
+        choice_B = data[generate_B]['follower_count']
+        if choice_A < choice_B:
+            return True
+    else:
+        print(f"Sorry, that's wrong. Final score {score}")
+        return False
+
+
 while playing:
     os.system('cls||clear')
 
-    generate_A = random.randint(0, len(data) - 1)
     print(logo)
+    generate_A = ''
+    generate_B = ''
+    generate_A, generate_B = generate(generate_A, generate_B)
 
     if correct == True:
+        score += 1
         print(f"You're right! Current score: {score}")
 
     print(f"Compare A: {data[generate_A]['name']}, a {data[generate_A]['description']}, from {data[generate_A]['country']}")
     print(vs)
-    generate_B = random.randint(0, len(data) - 1)
 
     if generate_A == generate_B:
         generate_B = random.randint(0, len(data))
     print(f"Against B: {data[generate_B]['name']}, a {data[generate_B]['description']}, from {data[generate_B]['country']}")
     
-
+    correct = checker()
     
-    user_guess = input("Who has more followers? Type 'A' or 'B': ")
-
-    if user_guess.lower() == 'a':
-        choice_A = data[generate_A]['follower_count']
-        choice_B = data[generate_B]['follower_count']
-        if choice_A > choice_B:
-            score += 1
-            correct = True
-        else:
-            print(f"Sorry, that's wrong. Final score {score}")
-            playing = False
-
-    elif user_guess.lower() == 'b':
-        choice_A = data[generate_A]['follower_count']
-        choice_B = data[generate_B]['follower_count']
-        if choice_A < choice_B:
-            score += 1
-            currect = True
-        else:
-            print(f"Sorry, that's wrong. Final score {score}")
-            playing = False
+    if correct == False:
+        playing = False
+    
+  
 
