b2da1126dfe866d23c61e99879b442c4ce2f117b
Oscar Solis <oscarsolis3301@gmail.com>
2022-04-07 15:27:24 -0600

    Improved

--- game.py
+++ game.py
@@ -5,15 +5,17 @@ import os
 
 playing = True
 score = 0
-correct = False
+correct = True
 
 
 def generate(generate_A, generate_B):
+    """Generates two random values from the start to the end of the list"""
     generate_A = random.randint(0, len(data) - 1)
     generate_B = random.randint(0, len(data) - 1)
     return generate_A, generate_B
 
 def checker():
+    """Checks the user's input and returns True if they got it correct, or False if incorrect"""
     user_guess = input("Who has more followers? Type 'A' or 'B': ")
     if user_guess.lower() == 'a':
         choice_A = data[generate_A]['follower_count']
@@ -28,12 +30,13 @@ def checker():
         choice_B = data[generate_B]['follower_count']
         if choice_A < choice_B:
             return True
-    else:
-        print(f"Sorry, that's wrong. Final score {score}")
-        return False
+        else:
+            print(f"Sorry, that's wrong. Final score {score}")
+            return False
 
 
-while playing:
+while correct:
+    """Main game loop"""
     os.system('cls||clear')
 
     print(logo)
@@ -41,8 +44,7 @@ while playing:
     generate_B = ''
     generate_A, generate_B = generate(generate_A, generate_B)
 
-    if correct == True:
-        score += 1
+    if correct == True and score != 0:
         print(f"You're right! Current score: {score}")
 
     print(f"Compare A: {data[generate_A]['name']}, a {data[generate_A]['description']}, from {data[generate_A]['country']}")
@@ -53,9 +55,11 @@ while playing:
     print(f"Against B: {data[generate_B]['name']}, a {data[generate_B]['description']}, from {data[generate_B]['country']}")
     
     correct = checker()
+    if correct == True:
+        score += 1
     
-    if correct == False:
-        playing = False
+    
+    
     
   
 
